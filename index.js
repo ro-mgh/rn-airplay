13cf7fb5a428dc6664c8b5677917d31ba5898ab5
ro-mgh <romyagchenkov@gmail.com>
2021-05-28 21:49:22 +0300

    Removal of airplay functional

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
 import React, { Component } from "react";
 import {
-  NativeEventEmitter,
   NativeModules,
   requireNativeComponent
 } from "react-native";
 
 const { RNAirplay } = NativeModules;
-export const Airplay = RNAirplay;
 export const AirplayButton = requireNativeComponent("RNAirplayButton", null);
-export const AirplayListener = new NativeEventEmitter(RNAirplay);
